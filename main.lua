89d027fa488d99c2cdbd615d8bdfa4f1c404e4b4
Nora <154567527+sollatholl@users.noreply.github.com>
2026-07-21 00:13:51 -0400

    Refactor variable names and improve function logic

--- main.lua
+++ main.lua
@@ -7,84 +7,73 @@ local BatRemotes = ReplicatedStorage:FindFirstChild("BatRemotes")
 local RunService = game:GetService("RunService")
 
 local LocalPlayer = Players.LocalPlayer
-local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:wait()
+local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
 LocalPlayer.CharacterAdded:Connect(function(char)
     Character = char
 end)
 
 local UltSpam = Iris.State(false)
 local KillAura = Iris.State(false)
-local NoCD = Iris.State(false)
+local NoCooldown = Iris.State(false)
 local KillAll = Iris.State(false)
-local LAG = Iris.State(false)
+local LagServer = Iris.State(false)
 
 -- Core
 local function mech()
     local one = Character:FindFirstChildWhichIsA("Tool") or LocalPlayer.Backpack:FindFirstChildWhichIsA("Tool")
-
-    local two
-    if one and BatRemotes then
-        two = BatRemotes:FindFirstChild(one.Name)
-    end
-
-    local three
+    local two = one and BatRemotes and BatRemotes:FindFirstChild(one.Name)
+    local three = nil
     if two and two:FindFirstChildWhichIsA("RemoteEvent") then
-        three = two:FindFirstChildWhichIsA("RemoteEvent")
+        three = two and two:FindFirstChildWhichIsA("RemoteEvent")
     end
 
-    if one and two and three then
-        return one, two, three
-    else
-        return error("Grabbing ERROR")
-    end
-end
-
-local function players()
-    local plrs = {}
-    for _, p in ipairs(Players:GetPlayers()) do
-        table.insert(plrs, p)
-    end
-
-    return plrs
+    return one, two, three 
 end
 
 RunService.Heartbeat:Connect(function()
-    if not KillAura.value and not UltSpam.value and not NoCD.value and not KillAll.value and not LAG.value then return end
+    if not KillAura.value and not UltSpam.value and not NoCooldown.value and not KillAll.value and not LagServer.value then return end
     local bat, hit, special = mech()
 
     if KillAura.value and hit then hit:FireServer(bat) end
     if UltSpam.value and special then special:FireServer(bat) end
-	if NoCD.value then
+
+	if NoCooldown.value and bat then
 		bat:SetAttribute("cooldown", 0)
 		bat:SetAttribute("ability_cooldown", 0)
 	end
-    if KillAll.value then
-        local ppl = players()
-        for _, v in ipairs(ppl) do
-            local char = v.Character or v.CharacterAdded:Wait()
-            local hrp = char and char:FindFirstChild("HumanoidRootPart")
 
-            local Event = BatRemotes["Electro Bat"].Electrify
-            Event:FireServer(
-                game:GetService("Players").LocalPlayer.Character["Electro Bat"],
-                hrp.Position
-            )
+    if KillAll.value and bat then
+        for _, v in ipairs(Players:GetPlayers()) do
+            local char = v.Character
+            local hrp = char and char:FindFirstChild("HumanoidRootPart")
+            
+            if char and hrp then
+                local Event = BatRemotes["Electro Bat"].Electrify
+                Event:FireServer(
+                    Character:FindFirstChild("Electro Bat"),
+                    hrp.Position
+                )
+            end
         end
     end
-    if LAG.value then
-        -- will finish later once i find those remotes.
+    if LagServer.value then
+        -- will add later on
     end
 end)
 
 -- UI
 Iris:Connect(function()
     Iris.Window({"Bat Game UI"})
-		Iris.Text({"Script by @z3zta"})
-		Iris.Checkbox({"Kill Aura"}, {isChecked = KillAura})
-		Iris.Checkbox({"Spam Ability"}, {isChecked = UltSpam})
-		Iris.Checkbox({"No Cooldowns"}, {isChecked = NoCD})
-        Iris.Checkbox({"Kill All"}, {isChecked = KillAll})
-        Iris.Checkbox({"Lag Server"}, {isChecked = LAG})
+        Iris.Text({"Script by @z3zta"})
+        Iris.SameLine()
+            Iris.Checkbox({"Kill Aura"}, {isChecked = KillAura})
+            Iris.Checkbox({"Spam Ability"}, {isChecked = UltSpam})
+            Iris.Checkbox({"No Cooldowns"}, {isChecked = NoCooldown})
+            Iris.Checkbox({"Kill All"}, {isChecked = KillAll})
         Iris.End()
+        
+        Iris.Separator()
+
+        Iris.Checkbox({"Lag Server"}, {isChecked = LagServer})
     Iris.End()
 end)
